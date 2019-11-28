857e528bd7713f612d8c203e6f30a5c62e34facb
Igor Nikolaev <igor.nikolaev@nikisoft.ru>
2019-11-28 22:53:39 +0200

    Add package.json and fix name

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+const ObjectId = (m = Math, d = Date, h = 16, s = s => m.floor(s).toString(h)) => s(d.now() / 1000) + ' '.repeat(h).replace(/./g, () => s(m.random() * h))
+
 module.exports.templateTags = [{
-    name: 'mongo-object-id',
-    displayName: 'Mongo ObjectId',
-    description: 'Mongo ObjectId',
+    name: 'mongo_object_id',
+    displayName: 'ObjectId',
+    description: 'generate Mongo ObjectId',
     args: [],
     async run (context) {
-        return (m = Math, d = Date, h = 16, s = s => m.floor(s).toString(h)) => s(d.now() / 1000) + ' '.repeat(h).replace(/./g, () => s(m.random() * h));
+        return ObjectId();
     }
 }];
